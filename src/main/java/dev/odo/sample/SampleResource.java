bab8a7cd9e24eaca78b9fcfa8ede0eeae1dad608
Adam Wisniewski <awisniew@us.ibm.com>
2020-11-02 19:58:48 -0500

    test update
    Signed-off-by: Adam Wisniewski <awisniew@us.ibm.com>

--- src/main/java/dev/odo/sample/SampleResource.java
+++ src/main/java/dev/odo/sample/SampleResource.java
@@ -23,6 +23,6 @@ public class SampleResource {
 
     @GET
     public String getRequest() {
-        return "Hello! Welcome to Open Liberty";
+        return "Hello WEBHOOK!! Welcome to Open Liberty";
     }
 }
